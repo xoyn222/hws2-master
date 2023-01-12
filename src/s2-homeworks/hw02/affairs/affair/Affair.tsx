3eaa4c6a2e30796419c945e884311e812f5709ea
xoyn222 <kostuk186@gmail.com>
2023-01-12 16:02:42 +0000

    hw2

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { AffairType } from '../../HW2'
+import {AffairType} from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number)=>void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-        // need to fix
+        props.deleteAffairCallback(props.affair._id)// need to fix
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -25,19 +25,17 @@ function Affair(props: AffairPropsType) {
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-
+                {props.affair.name}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {props.affair.priority}
             </div>
-
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
                 // need to fix
-
-            >
+                onClick={deleteCallback}>
                 {/*текст кнопки могут изменить студенты*/}
                 X
                 {/**/}
